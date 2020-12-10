dbaa0a75be440d40a0bc2a2eee31d16f7342ce1e
GoodNight <goodnightlilanhui@gmail.com>
2020-12-10 17:32:44 +0800

    perf: footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- Footer Start -->
-  <footer class="footer">
+  <!-- <footer class="footer"> -->
+  <footer class="footer-custom">
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
@@ -13,3 +14,16 @@
   </footer>
   <!-- end Footer -->
 </template>
+<style type="text/css">
+.footer-custom {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 250px !important;
+  padding: 19px 15px 20px;
+  color: #6c757d;
+}
+.left-side-menu-condensed .footer-custom {
+  left: 70px !important;
+}
+</style>
