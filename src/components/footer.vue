338a6a450c7243c64b8887db648423e1557a338e
GoodNight <goodnightlilanhui@gmail.com>
2020-12-28 15:03:48 +0800

    style: footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,10 +5,13 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
+          2021 &copy; Xidian University All Rights Reserved.
+        </div>
+        <!-- <div class="col-12">
           2019 &copy; Shreyu. All Rights Reserved. Crafted with
           <i class="uil uil-heart text-danger font-size-12"></i> by
           <a href="https://coderthemes.com" target="_blank">Coderthemes</a>
-        </div>
+        </div> -->
       </div>
     </div>
   </footer>
